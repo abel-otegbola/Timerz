3b97f45638e583d5148b46a530c73a01d4f0cd3c
abel-otegbola <abel15655@gmail.com>
2022-07-04 22:20:11 +0100

    Removed config file

--- src/index.js
+++ src/index.js
@@ -3,13 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { ColorModeScript } from '@chakra-ui/react';
-import theme from './config/themeConfig';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
     <App />
   </React.StrictMode>
 );
